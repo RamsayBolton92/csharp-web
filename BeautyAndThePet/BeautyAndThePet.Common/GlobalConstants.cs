b0cf11760e9ad4083eaf2d80821daa735ab8a138
faust92 <fausts@abv.bg>
2020-12-19 09:36:09 +0200

    Fix Index

--- BeautyAndThePet/BeautyAndThePet.Common/GlobalConstants.cs
+++ BeautyAndThePet/BeautyAndThePet.Common/GlobalConstants.cs
@@ -7,14 +7,14 @@
         public const string AdministratorRoleName = "Administrator";
 
         public const string AboutUs = 
-            "The population of homeless animals in Bulgaria is enormous." + 
-            "It is caused by owners not taking care of their animals sexual desires or their solution is castrating them" +
-            "which makes them mentally ill and scared. I will not agree this is right decision." +
-            "Otherwise not taking care leads to accidental pregnancy and mixed breed puppies which no one wants to take care of" +
-            "because they don't look as well as single breed animals. People murder them or kick them out. Then other people kill them" +
-            "Not killing them is a risk also because there are many incidents" +
-            "involving them and then the government castrate or kills homeless animals" +
-            "So I decided to take care of animals sexual desires by building a dating platform so their owners" +
+            "The population of homeless animals in Bulgaria is enormous. " + 
+            "It is caused by owners not taking care of their animals sexual desires or their solution is castrating them " +
+            "which makes them mentally ill and scared. I will not agree this is right decision. " +
+            "Otherwise not taking care leads to accidental pregnancy and mixed breed puppies which no one wants to take care of " +
+            "because they don't look as well as single breed animals. People murder them or kick them out. Then other people kill them " +
+            "Not killing them is a risk also because there are many incidents " +
+            "involving them and then the government castrate or kills homeless animals. " +
+            "So I decided to take care of animals population by building a dating platform so their owners " +
             "can easilly connect and make single breed animals. Then they can split them and sell them and make a profit of it.";
 
         public const string Rights = "All rights reserved";
